1a1784d9dcc8fd28bf0a34335c9f6ab95fce7d85
cmcorrales <cmcorrales3@gmail.com>
2018-10-16 13:57:47 -0400

    refactored SelectedRestaurant to prevent errors when selecting restaurant with empty menu

--- src/components/SelectedRestaurant.js
+++ src/components/SelectedRestaurant.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Segment, Rating, Button, Icon, Label } from 'semantic-ui-react';
+import { Segment, Rating, Button, Icon, Label, Modal } from 'semantic-ui-react';
 import { addReview } from '../actions/restaurants';
 import NavigationBar from './NavigationBar'
+import GiveFeedbackModal from './GiveFeedbackModal';
 import axios from 'axios';
 
 class SelectedRestaurant extends React.Component {
   state = {
     dishes: [],
     rating: '',
-    dishId: ''
+    dishId: '',
+    open: false,
   }
+
+  show = dimmer => () => this.setState({ dimmer, open: true })
+  close = () => this.setState({ open: false })
 
   componentDidMount() {
     const menusArray = this.props.selectedRestaurant.selectedRestaurant.menus[0]
-    const currentMenu = menusArray ? menusArray.dishes.map(dish => dish) : "There are no menus to display"
+    const currentMenu = menusArray ? menusArray.dishes.map(dish => dish) : null;
     this.setState({
       dishes: currentMenu
     })
@@ -32,21 +37,45 @@ class SelectedRestaurant extends React.Component {
     })
   }
 
+  displayDishes = (dishes) => {
+    const { open, dimmer } = this.state
+    return dishes.map(dish => {
+      console.log("dish:", dish.name)
+      return (
+        <div>
+            <Segment color='red' raised>{dish.name}<br/>
+              <Rating icon='star' defaultRating={0} maxRating={4} size='massive' onRate={this.handleRate} onClick={() => this.getDish(this.props.review)}/><br/>
+              <Button onClick={this.show('blurring')} color='red'>give feedback </Button><Button color='blue' onClick={this.show('blurring')}>view feedback </Button>
+            </Segment>
+            <Modal dimmer={dimmer} open={open} onClose={this.close}>
+              <GiveFeedbackModal />
+            </Modal>
+        </div>
+      )
+    })
+  }
+
+  checkDishes = (dishes) => {
+    if (dishes) {
+      return this.displayDishes(dishes)
+    }
+    else {
+      return (<div>
+              <Segment>This restaurant does not have any menu items to display.</Segment>
+            </div>
+          )
+    }
+  }
+
   render() {
-    console.log(this.props.review)
+    console.log('dishes:',this.state.dishes)
     // console.log(this.props.selectedRestaurant.selectedRestaurant.menus[0].dishes.map(dish => dish.name))
     return (
       <React.Fragment>
         <NavigationBar />
         <div className="menuItems">
           <h1 className="heading">{this.props.selectedRestaurant.selectedRestaurant.name}</h1>
-          {this.state.dishes.map(dish => {
-            return <Segment color='red' raised>{dish.name}<br/>
-                <Rating icon='star' defaultRating={0} maxRating={4} size='massive' onRate={this.handleRate} onClick={() => this.getDish(this.props.review)}/><br/>
-                <Button color='red'>view feedback </Button><Button color='blue'>give feedback </Button>
-
-              </Segment>
-          })}
+          { this.checkDishes(this.state.dishes) }
         </div>
       </React.Fragment>
     )

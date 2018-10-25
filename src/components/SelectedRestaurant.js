86ea2b9897884e0801448e24a3df615ad0871315
cmcorrales <cmcorrales3@gmail.com>
2018-10-25 10:48:15 -0400

    add popovers

--- src/components/SelectedRestaurant.js
+++ src/components/SelectedRestaurant.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Segment, Rating, Button, Icon, Label, Modal } from 'semantic-ui-react';
+import { Segment, Rating, Button, Icon, Label, Modal, Dropdown, Menu, Statistic, Header } from 'semantic-ui-react';
 import { addRating } from '../actions/restaurants';
 import NavigationBar from './NavigationBar'
 import GiveFeedbackModal from './GiveFeedbackModal';
@@ -14,6 +14,7 @@ class SelectedRestaurant extends React.Component {
     rating: '',
     dishId: '',
     dishName: '',
+    type: '',
   }
 
   show = (dimmer, dishName, dishId) => () => this.setState({ dimmer, open: true, dishName: dishName, dishId: dishId })
@@ -37,13 +38,23 @@ class SelectedRestaurant extends React.Component {
     this.props.onAddRating(rating, dishId)
   }
 
-
-  getAverageDishRating = (filteredDishes) => {
+  getReviewCount = (filteredDishes) => {
     if (filteredDishes === undefined || filteredDishes.length == 0) {
       return "No ratings"
       //this return the message above and an empty array? multiple times
     } else {
       //console.log("The array has something in it! Result is: " + this.props.filteredDishes["0"])
+      const flavorArray = filteredDishes["0"].reviews.map(item => item.rating)
+      const filteredFlavorArray = flavorArray.filter(value => value !== null )
+      return filteredFlavorArray.length
+    }
+  }
+
+
+  getAverageDishRating = (filteredDishes) => {
+    if (filteredDishes === undefined || filteredDishes.length == 0) {
+      return "No ratings"
+    } else {
       const flavorArray = filteredDishes["0"].reviews.map(item => item.rating)
       const filteredFlavorArray = flavorArray.filter(value => value !== null )
       const averageRating = filteredFlavorArray.reduce((a, b) => a + b, 0) / (filteredFlavorArray.length||1)
@@ -71,18 +82,22 @@ class SelectedRestaurant extends React.Component {
               <Label className='label-position' as='a' color='red' ribbon>
                 {this.getAverageDishRating(this.filteredDishes(dish.id))} <Icon name='star' />
               </Label>
-              <span>{dish.name}</span><br/>
-              <Rating icon='star' defaultRating={0} maxRating={5} size='massive' onRate={(e, {rating}) => this.handleRate(e, rating, dish.id, dish.name)} /><br/>
+              <span><Header as='h2'>{dish.name}</Header></span><br/>
+              <div className='rating-container'><Rating icon='star' defaultRating={0} maxRating={5} size='massive' onRate={(e, {rating}) => this.handleRate(e, rating, dish.id, dish.name)} /></div><br/>
               <Button onClick={this.show('blurring', dish.name, dish.id)} color='red'>give feedback</Button>
               <Modal trigger={<Button color='blue'>view feedback</Button>}>
                 <ViewFeedbackModal filteredDishes={this.filteredDishes(dish.id)} dishName={dish.name}/>
               </Modal>
+              <div class='reviews-count'>
+              <Statistic size='mini'>
+                <Statistic.Value>{this.getReviewCount(this.filteredDishes(dish.id))}</Statistic.Value>
+                <Statistic.Label>Reviews</Statistic.Label>
+              </Statistic>
+            </div>
             </Segment>
             <Modal dimmer={dimmer} open={open} onClose={this.close}>
               <GiveFeedbackModal dishName={this.state.dishName} dishId={this.state.dishId} onClose={this.close}/>
             </Modal>
-
-
         </div>
       )
     })
@@ -104,15 +119,77 @@ class SelectedRestaurant extends React.Component {
     return this.props.dishes.filter(dish => dish.id === dishId)
   }
 
+  onChangeType = (event) => {
+    console.log(event.target.value)
+    this.setState({
+      type: event.target.value
+    })
+    this.sortBy(event)
+    // sortDishes()
+  }
+
+  // sortDishes(event) {
+  //   const sortType = this.state.filters.type
+  //   const filteredDishes = allPets.filter(pet => pet.type === petType)
+  //
+  //   if (petType === 'all') {
+  //     this.setState({
+  //       pets: allPets
+  //     })
+  //   } else {
+  //     this.setState({
+  //       pets: filteredPets
+  //     })
+  //   }
+  // }
+
+  sortBy = (event) => {
+    if (event.target.value === 'alphabetical') {
+      return this.sortByAlphabetical()
+    }
+  }
+
+  sortByAlphabetical = () => {
+    this.setState((prevState) => {
+      const sortedDishes = prevState.dishes.sort((dish1, dish2) => {
+        let name2;
+        if (dish1.name && dish2.name) {
+          const name1 = dish1.name;
+          const name2 = dish2.name;
+          if(name1.toLowerCase() < name2.toLowerCase()) { return -1; }
+          if(name1.toLowerCase() > name2.toLowerCase()) { return 1; }
+          }
+        return 0;        // return name1.localeCompare(name2)
+      })
+      return {
+        dishes: sortedDishes
+      }
+    })
+  }
+
   render() {
+    const options = [
+      { key: 1, text: 'rating', value: 1 },
+      { key: 2, text: 'popularity', value: 2 },
+      { key: 3, text: 'alphabetical', value: 3 },
+    ]
     console.log("dishes:", this.props.dishes)
     console.log("filtered dishes:",this.filteredDishes())
     return (
       <React.Fragment>
         <NavigationBar />
         <div className="menuItems">
-          <h1 className="heading">{this.props.selectedRestaurant.selectedRestaurant.name}</h1>
-          { this.checkDishes(this.props.dishes) }
+          <div className="restaurant-header">
+            <h1 className="heading">{this.props.selectedRestaurant.selectedRestaurant.name}</h1>
+            <select onChange= {this.onChangeType}>
+              <option value="0">sort by</option>
+              <option value="alphabetical">alphabetical</option>
+              <option value="rating">rating</option>
+              <option value="popularity">popularity</option>
+              {/* <Dropdown onChange={ this.onChangeType } placeholder='sort by' fluid search selection options={options} /> */}
+            </select>
+          </div>
+          { this.checkDishes(this.state.dishes) }
           <AddDish />
         </div>
       </React.Fragment>
